81f22c31bbb728fa2ebfd65bb39522d4e152cf0e
srdothunter <77256808+srdothunter@users.noreply.github.com>
2021-12-14 16:20:33 +0800

    remove address

--- public/contractAddress.js
+++ public/contractAddress.js
@@ -1,6 +1,4 @@
 const mainAddress = {
-    // market: "5DyTrnYRYffHXWZiMUSsnvBKjyunxcfT1pCf4PHR2jA1yQwB" ,
-    // statistics:'5GMufcYoW7RyYPsYhNfu9P1EkWMahJmaRCuozY9RL3gAEGNY',
     basepath:'47.242.250.114:8082',
     name:'Apron'
 };
